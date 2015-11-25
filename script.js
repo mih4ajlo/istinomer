fd0c05f86eb24fe6d89a1f56445a78c884a5781a
Marta Jorgacevic <martajorgacevic@gmail.com>
2015-11-25 21:49:30 +0100

    istinomer - start

---
+++ script.js
@@ -0,0 +1,81 @@
+// function hnmaxHeight() {
+//     var maxHeight = 0;
+
+//     $('.hn-col-1').each(function() {
+//      maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+//    });
+
+//    $('.hn-col-1').each(function() {
+//      $(this).height(maxHeight);
+//    });
+// };
+
+function setDimensions() { /*.cd-hot-news-1, */
+	// if (jQuery(".cd-hot-news-1").css('display') === 'block') {
+	// 	jQuery(".cd-hot-news-1").css("display", "none");
+	// } else if (jQuery(".cd-hot-news-1").css('display') === 'none') {
+	// 	jQuery(".cd-hot-news-1").css("display", "block");
+	// }
+    jQuery(".hot-news-wrapper, .btn-left, .btn-right").css("height", jQuery("#hnews-position").outerHeight()); /*treba .hn-current*/ /*#hnews-position*/
+    jQuery(".hot-news-wrapper, .hn-col-wrap").css("width", jQuery(".cd-hot-news-1").outerWidth()-jQuery(".btn-left").outerWidth()*2-3);
+
+
+    // jQuery(".content-right").css("min-height", jQuery(".content-left").outerHeight());
+
+    if (jQuery(window).width() <= 768) {
+            jQuery(".hn-col-1").css("float", "none");
+            jQuery(".hn-col-1").css("width", "100%");
+    } else {
+        jQuery(".hn-col-1").css("float", "left");
+    }
+
+    window.onload = setDimensions;
+}
+
+function fontSize() {
+	// console.log(jQuery(".cd-top-news-1").css("height"));
+	jQuery(".top-news-in").each(function() {
+		if (jQuery(this).height() > 300) {
+			jQuery(this).find(".headline").animate({
+				fontSize : '20px'
+			}, "fast");
+			jQuery(this).find(".headline").css("line-height", "30px");
+		}
+	});
+
+	window.onload = fontSize;
+}
+jQuery(document).ready(function() {
+	// hnmaxHeight();
+    setDimensions();
+    fontSize();
+    // toggleCurrent();
+});
+
+jQuery(window).resize(function() {
+	// hnmaxHeight();
+    setDimensions();
+});
+
+// $( document ).ready(function() {
+//     $( ".container" ).each(function() {
+//         var newHeight = 0, $this = $( this );
+//         $.each( $this.children(), function() {
+//             newHeight += $( this ).height();
+//         });
+//         $this.height( newHeight );
+//     });
+// });
+
+// function toggleCurrent() {
+// 	$('.hn-col-wrap').toggle(function() {
+// 	    $(this)
+// 	        .addClass('hn-current')
+// 	        console.log("lalal");
+// 	    return false;
+// 	}, function() {
+// 	    $(this)
+// 	        .addClass('hn-current')
+// 	    return false;
+// 	});
+// };
